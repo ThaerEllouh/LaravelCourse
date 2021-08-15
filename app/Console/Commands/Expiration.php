a437f13dfd9c2062b381f0d773c04834dbbafa4b
ThaerEllouh <eng.thaeryousefellouh@gmail.com>
2021-08-16 02:06:35 +0300

    =add command folder for schedule task

---
+++ app/Console/Commands/Expiration.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class Expiration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:expire';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Expire Useres Every 5 Minutes automatically';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // return 0;
+        $users = User::where('expire', 1)->get();
+        foreach($users as $user){
+            $user->update(['expire' => 0]);
+        }
+
+    }
+}
